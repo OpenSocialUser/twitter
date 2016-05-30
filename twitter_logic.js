080968240c51d45504f4bb40ca9926228fdd7ff9
OpenSocialUser <opensocialuser2015@gmail.com>
2016-05-30 15:01:35 +0300

    Added SAP twitter timelines

--- twitter_logic.js
+++ twitter_logic.js
@@ -47,8 +47,13 @@ function renderEditPage() {
         html += "</select>";
     } else {
         html += "<select id='timeline_select'>";
-        html += "<option value='test' selected>Test</option>";
-        html += "<option value='hybris'>Hybris</option>";
+        html += "<option value='saphybris'>@saphybris</option>";
+        html += "<option value='SAPPHIRENOW'>@SAPPHIRENOW</option>";
+        html += "<option value='hybris_software'>@hybris_software</option>";
+        html += "<option value='SAPSocial'>@SAPSocial</option>";
+        html += "<option value='saphcp'>@saphcp</option>";
+        html += "<option value='BillRMcDermott'>@BillRMcDermott</option>";
+        html += "<option value='SAP'>@SAP</option>";
         html += "</select>";
     }
 
@@ -80,8 +85,13 @@ function saveTimeline() {
 }
 
 function insertTimeline(timeline) {
-    var testTimeline = "<a class='twitter-timeline' href='https://twitter.com/OpenSocialUser' data-widget-id='717304595446439936'>Твиты от @OpenSocialUser</a>";
-    var hybrisTimeline = "<a class='twitter-timeline'  href='https://twitter.com/saphybris' data-widget-id='702191997369643009'>Tweets by @saphybris</a>";
+    var saphybris = "<a class='twitter-timeline' href='https://twitter.com/saphybris' data-widget-id='727823334650593281'>Tweets by @saphybris</a>";
+    var SAPPHIRENOW = "<a class='twitter-timeline' href='https://twitter.com/SAPPHIRENOW' data-widget-id='737250519270498305'>Tweets by @SAPPHIRENOW</a>";
+    var hybris_software = "<a class='twitter-timeline' href='https://twitter.com/hybris_software' data-widget-id='737250981742817280'>Tweets by @hybris_software</a>";
+    var SAPSocial = "<a class='twitter-timeline' href='https://twitter.com/SAPSocial' data-widget-id='737251282608619520'>Tweets by @SAPSocial</a>";
+    var saphcp = "<a class='twitter-timeline' href='https://twitter.com/saphcp' data-widget-id='737251552482693121'>Tweets by @saphcp</a>";
+    var BillRMcDermott = "<a class='twitter-timeline' href='https://twitter.com/BillRMcDermott' data-widget-id='737251873682526208'>Tweets by @BillRMcDermott</a>";
+    var SAP = "<a class='twitter-timeline' href='https://twitter.com/SAP' data-widget-id='737252109201092608'>Tweets by @SAP</a>";
 
     var state = wave.getState();
     var timeline = state.get('timeline');
@@ -89,10 +99,20 @@ function insertTimeline(timeline) {
     var html = "";
     var htmlFooter = "";
 
-    if (timeline == "test") {
-        html += testTimeline;
-    } else if (timeline == "hybris") {
-        html += hybrisTimeline;
+    if (timeline == "saphybris") {
+        html += saphybris;
+    } else if (timeline == "SAPPHIRENOW") {
+        html += SAPPHIRENOW;
+    } else if (timeline == "hybris_software") {
+        html += hybris_software;
+    } else if (timeline == "SAPSocial") {
+        html += SAPSocial;
+    } else if (timeline == "saphcp") {
+        html += saphcp;
+    } else if (timeline == "BillRMcDermott") {
+        html += BillRMcDermott;
+    } else if (timeline == "SAP") {
+        html += SAP;
     }
 
     if (isOwner) {
@@ -141,7 +161,7 @@ function renderTwitter() {
 
     var iframeHeight = 0;
     if (twitterWidgetHeight == 0) {
-        iframeHeight = 500;
+        iframeHeight = 520;
     } else {
         iframeHeight = twitterWidgetHeight;
     }
